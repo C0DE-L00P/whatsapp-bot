5ef82f5450adf0a170743281138cd1851e6732d1
Mohamed Gamal <jaiden90@gmail.com>
2022-09-26 13:22:21 +0200

    - Interval test

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
 });
 
+let i = 0;
 client.on('ready', () => {
     console.log('Client is ready!');
+    setInterval(()=>{
+        i++;
+        console.log('sent interval '+ i);
+        client.sendMessage("201020624331@c.us", 'ازيك يا اشرف '+ Date.now());
+    },1000*30)
 });
 client.on('message', message => {
 	if(message.body === '!ping') {
 		// message.reply('pong');
+        console.log('from',message.from)
         client.sendMessage(message.from, 'pong');
 	}
 });
