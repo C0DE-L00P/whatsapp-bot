19c2f65ea1ff489c2abfb6c34259dabc3b5c7740
Mohamed Gamal <jaiden90@gmail.com>
2023-07-24 18:25:36 +0300

    Adding session strategy to save session

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const qrcode = require('qrcode-terminal');
+const { Client, LegacySessionAuth, LocalAuth } = require('whatsapp-web.js');
 
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+
+const client = new Client({
+    authStrategy: new LocalAuth({
+        clientId: "client-one"
+    })
+});
+
+// Save session values to the file upon successful auth
+client.on('authenticated', (session) => {
+    console.log('logged in', session)
+});
+
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+    qrcode.generate(qr, { small: true });
 });
 
-let i = 0;
 client.on('ready', () => {
-    console.log('Client is ready!');
-    setInterval(()=>{
-        i++;
-        console.log('sent interval '+ i);
-        client.sendMessage("201020624331@c.us", 'ازيك يا اشرف '+ i + " " + Date.now());
-    },1000*30)
+    client.sendMessage("201020624331@c.us", 'DO NOT EDIT THIS LINE ' + getCurrentDate());
+
 });
+
 client.on('message', message => {
-	if(message.body === '!ping') {
-		// message.reply('pong');
-        console.log('from',message.from)
-        client.sendMessage(message.from, 'pong');
-	}
+    if (message.body === 'السود عيونه') {
+        // message.reply('pong');
+        console.log('from', message.from)
+        client.sendMessage(message.from, 'يا ولاه');
+    }
 });
 
 client.initialize();
+
+// (Optional)
+function getCurrentDate() {
+    // Set the timezone to Cairo/Africa
+    const options = { timeZone: 'Africa/Cairo', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+    const today = new Date();
+    return today.toLocaleString('en-US', options).replace(',', '');
+}
